8b9874849b16b0017435c879d4765a8e62868948
Marco Colombo <mar.colombo13@gmail.com>
2024-11-13 21:37:11 +0100

    Support working on an unattached namespace.

--- R/fuzz.R
+++ R/fuzz.R
@@ -2,6 +2,7 @@ get_exported_functions <- function(package, ignore.names = NULL) {
   funs <- sort(getNamespaceExports(package))
   funs <- grep(".__", funs, fixed = TRUE, invert = TRUE, value = TRUE)
   funs <- setdiff(funs, ignore.names)
+  attr(funs, "package") <- package
   return(funs)
 }
 
@@ -15,13 +16,21 @@ fuzz <- function(funs, what, ignore.patterns = NULL,
     if (count > 0) return(invisible())
     cat("\U0001f3c3 You didn't get caught by the fuzz!\n")
   }
+  getter <- function() {
+    if (is.null(package))
+      get
+    else
+      function(x) getFromNamespace(x, package)
+  }
 
   count <- 0
   ignore.patterns <- paste0(c(ignore.patterns,
                               "is missing, with no default"),
                             collapse = "|")
+  package <- attr(funs, "package")
   for (f in funs) {
-    tryCatch(capture.output(suppressMessages(get(f)(what))),
+    fun <- getter()(f)
+    tryCatch(capture.output(suppressMessages(fun(what))),
              error = function(e) {
                if (!grepl(f, e) &&
                    !grepl(ignore.patterns, e)) {
