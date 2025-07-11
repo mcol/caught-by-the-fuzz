5e8d18a9f59b9681e082403b0b72907cf38ab4fa
Marco Colombo <mar.colombo13@gmail.com>
2025-07-11 17:11:32 +0200

    Add some more tests.

--- tests/testthat/test_fuzz.R
+++ tests/testthat/test_fuzz.R
@@ -33,3 +33,36 @@ test_that("check functionality", {
                 pass.msg)
   rm("with.readline", envir = .GlobalEnv)
 })
+
+test_that("check classes returned", {
+  testthat::skip_on_cran()
+
+  SW({
+  expect_what(fuzz("list", NULL),
+              "NULL")
+  expect_what(fuzz("list", NA),
+              "NA")
+  expect_what(fuzz("list", data.frame()),
+              "data.frame()")
+  expect_what(fuzz("list", list()),
+              "list()")
+  expect_what(fuzz("list", 1:3),
+              "1:3")
+  expect_what(fuzz("list", letters),
+              "letters")
+
+  ## TODO: this seems unfortunate, could it be made to return `letters`?
+  what <- letters
+  expect_what(fuzz("list", what),
+              "what")
+  })
+})
+
+test_that("self fuzz", {
+  testthat::skip_on_cran()
+
+  expect_output(fuzz("fuzz", list()),
+                "'funs' should be of class character")
+  expect_output(fuzz("fuzz", NULL),
+                "'funs' should be of class character")
+})
