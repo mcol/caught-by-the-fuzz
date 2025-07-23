mcol/caught-by-the-fuzz

commit 3abdc71789b8dcc1b031476bab3831a3cdc13795

Validate the ignore.patterns argument and improve its documentation.

R/fuzz.R
@@ -61,8 +61,9 @@ get_exported_functions <- function(package, ignore.names = NULL) {
 #'        in its listified version (`FALSE` by default). When set to `TRUE`,
 #'        if `what` is `list(x = x)`, the function will operate as if `what`
 #'        were `list(x = x, "list(x)" = list(x))`, for any input object `x`.
-#' @param ignore.patterns A character string containing a regular expression
-#'        to match the messages to ignore.
+#' @param ignore.patterns One or more strings containing regular expressions
+#'        to match the errors to ignore. The string "is missing, with no
+#'        default" is always ignored.
 #' @param ignore.warnings Whether warnings should be ignored (`FALSE` by
 #'        default).
 #'
@@ -72,7 +73,7 @@ get_exported_functions <- function(package, ignore.names = NULL) {
 #'
 #' @export
 fuzz <- function(funs, what = input_list, package = NULL, listify.what = FALSE,
-                 ignore.patterns = NULL, ignore.warnings = FALSE) {
+                 ignore.patterns = "", ignore.warnings = FALSE) {
 
   ## input validation
   validate_class(funs, "character")
@@ -83,10 +84,16 @@ fuzz <- function(funs, what = input_list, package = NULL, listify.what = FALSE,
     validate_class(package, "character")
   }
   validate_class(listify.what, "logical")
+  validate_class(ignore.patterns, "character")
 
   ## expand the set of inputs with their listified version
   if (listify.what)
     what <- append_listified(what)
+
+  ## join all regular expression patterns
+  ignore.patterns <- paste0(c(ignore.patterns,
+                              "is missing, with no default"),
+                            collapse = "|")
 
   ## loop over the inputs
   runs <- list()
@@ -149,9 +156,6 @@ fuzzer <- function(funs, what, what.char = "", package = NULL,
     function(x) utils::getFromNamespace(x, package)
   }
 
-  ignore.patterns <- paste0(c(ignore.patterns,
-                              "is missing, with no default"),
-                            collapse = "|")
   out.res <- lapply(funs, function(x) {
     data.frame(fun = x, res = "OK", msg = "")
   })

man/fuzz.Rd
@@ -9,7 +9,7 @@ fuzz(
   what = input_list,
   package = NULL,
   listify.what = FALSE,
-  ignore.patterns = NULL,
+  ignore.patterns = "",
   ignore.warnings = FALSE
 )
 }
@@ -35,8 +35,9 @@ in its listified version (\code{FALSE} by default). When set to \code{TRUE},
 if \code{what} is \code{list(x = x)}, the function will operate as if \code{what}
 were \code{list(x = x, "list(x)" = list(x))}, for any input object \code{x}.}
 
-\item{ignore.patterns}{A character string containing a regular expression
-to match the messages to ignore.}
+\item{ignore.patterns}{One or more strings containing regular expressions
+to match the errors to ignore. The string "is missing, with no
+default" is always ignored.}
 
 \item{ignore.warnings}{Whether warnings should be ignored (\code{FALSE} by
 default).}

tests/testthat/test_fuzz.R
@@ -11,6 +11,8 @@ test_that("input validation", {
                "'funs' is empty")
   expect_error(fuzz("list", list()),
                "'what' is empty")
+  expect_error(fuzz("list", ignore.patterns = TRUE),
+               "'ignore.patterns' should be of class character")
 })
 
 test_that("check skipped functions", {

commit 1e7c583b37a97fde5e074b979917b7c966eead3d

Fix the generation of ignored patterns.
This fixes a regression from 3abdc717, as the default value was changed
from NULL to "", and this caused an extra | to be added at the start of
the regular expression when the default ignore.patterns was used.

R/fuzz.R
@@ -99,6 +99,7 @@ fuzz <- function(funs, what = input_list, package = NULL, listify.what = FALSE,
   ignore.patterns <- paste0(c(ignore.patterns,
                               "is missing, with no default"),
                             collapse = "|")
+  ignore.patterns <- gsub("^\\|", "", ignore.patterns) # remove extra |
 
   ## loop over the inputs
   runs <- list()